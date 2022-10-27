a06ec0bf259e17e7031d863784583cbd273c1a98
LucaPallas <101177414+LucaPallas@users.noreply.github.com>
2022-10-28 06:19:19 +0800

    UI stuff

---
+++ app/src/main/java/com/example/metaMergeTasker/passwordGeneratorScreen.java
@@ -0,0 +1,14 @@
+package com.example.metaMergeTasker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class passwordGeneratorScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_generator_screen);
+    }
+}
