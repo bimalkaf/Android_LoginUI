aa1507dfa25ff06d231f0fc636b1dc310fd7e1eb
Adam <70795583+AdamM-AU@users.noreply.github.com>
2022-11-07 12:14:43 +1100

    Added rough in UI template for notes, added further actions to homeScreen and introduced Toast message boxes to yet to be implemented features

--- app/src/main/java/com/example/metaMergeTasker/homeScreen.java
+++ app/src/main/java/com/example/metaMergeTasker/homeScreen.java
@@ -24,6 +24,9 @@ public class homeScreen extends AppCompatActivity {
         // Adam: Create button object pointers, so we can use them :D
         ImageButton pwGen = (ImageButton) findViewById(R.id.btn_generator);
         ImageButton lists = (ImageButton) findViewById(R.id.btn_lists);
+        ImageButton organiser = (ImageButton) findViewById(R.id.btn_organiser);
+        ImageButton expMang = (ImageButton) findViewById(R.id.btn_expense);
+        ImageButton pwMang = (ImageButton) findViewById(R.id.btn_manager);
 
         // Adam: PWGen Image Button Action
         pwGen.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +43,29 @@ public class homeScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Adam: Organiser Button Action
+        organiser.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                messageBox("Organiser - Yet to be implemented!");
+            }
+        });
+        // Adam: Expense Manager Button Action
+        expMang.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                messageBox("Expense Manager - Yet to be implemented!");
+            }
+        });
+        // Adam: Password Manager Button Action
+        pwMang.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                messageBox("Password Manager - Yet to be implemented!");
+            }
+        });
+    }
+
+    // Adam: Toast Message Box :)
+    public void messageBox(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/metaMergeTasker/noteMainActivity.java
+++ app/src/main/java/com/example/metaMergeTasker/noteMainActivity.java
@@ -12,5 +12,15 @@ public class noteMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_main_activity);
+
+        //
+        TextView pageTitle =(TextView) findViewById(R.id.noteTextView);
+
+        // Adam: We set the page title here incase we want an easy way to translate to other languages in
+        // the future
+        String pageTitleText = "Notes";
+        pageTitle.setText(pageTitleText);
+
+
     }
 }
