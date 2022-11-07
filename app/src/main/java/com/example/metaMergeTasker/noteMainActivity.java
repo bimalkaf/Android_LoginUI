9564b1da5a3bed25c2e4094c0ad641a48bce42c1
Adam <70795583+AdamM-AU@users.noreply.github.com>
2022-11-07 13:22:35 +1100

    Note Take Completed! Ready for Template to be applied

--- app/src/main/java/com/example/metaMergeTasker/noteMainActivity.java
+++ app/src/main/java/com/example/metaMergeTasker/noteMainActivity.java
@@ -1,26 +1,97 @@
 package com.example.metaMergeTasker;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.metaMergeTasker.R.*;
 
 import android.os.Bundle;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.button.MaterialButton;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+
 public class noteMainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_main_activity);
+        setContentView(layout.activity_note_main_activity);
+        Button addNoteBtn = (Button) findViewById(id.addNoteBtn);
+        ListView listView = findViewById(id.noteListView);
 
-        //
-        TextView pageTitle =(TextView) findViewById(R.id.noteTextView);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.metaMergeTasker", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
 
-        // Adam: We set the page title here incase we want an easy way to translate to other languages in
-        // the future
-        String pageTitleText = "Notes";
-        pageTitle.setText(pageTitleText);
+        if (set == null) {
+            notes.add("Example note");
+        } else {
+            notes = new ArrayList(set);
+        }
 
+        // Using custom listView Provided by Android Studio
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, notes);
 
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Going from MainActivity to NotesEditorActivity
+                Intent intent = new Intent(getApplicationContext(), noteEditActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+                // To delete the data from the App
+                new AlertDialog.Builder(noteMainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.metaMergeTasker", Context.MODE_PRIVATE);
+                                HashSet<String> set = new HashSet(noteMainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                            }
+                        }).setNegativeButton("No", null).show();
+                return true;
+            }
+        });
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Going from MainActivity to NotesEditorActivity
+                Intent intent = new Intent(getApplicationContext(), noteEditActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
