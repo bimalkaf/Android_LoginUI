4950f3e330563655038c4f9bddff02ecfeecc5bc
Adam <70795583+AdamM-AU@users.noreply.github.com>
2022-11-07 17:30:55 +1100

    cleaned up login code

--- app/src/main/java/com/example/metaMergeTasker/loginScreen.java
+++ app/src/main/java/com/example/metaMergeTasker/loginScreen.java
@@ -5,45 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.button.MaterialButton;
+import com.example.metaMergeTasker.extraFunctions;
 
-public class loginScreen extends AppCompatActivity
-{
+public class loginScreen extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        // ADAM: Fixed object types...
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        TextView forgotPass = (TextView) findViewById(R.id.forgotPass);
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
+        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.login_btn);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.login_btn);
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        //admin and admin
+            }
+        });
 
-        loginbtn.setOnClickListener(new View.OnClickListener()
-        {
-
-            public void onClick(View v)
-            {
-                if(username.getText().toString().equals(getString(R.string.username)) && password.getText().toString().equals(getString(R.string.password)))
-                {
+        // HARDCODED CREDENTIALS: admin and admin
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (username.getText().toString().equals(getString(R.string.username)) && password.getText().toString().equals(getString(R.string.password))) {
                     //correct
-                    Toast.makeText(loginScreen.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                    new extraFunctions.ShowToast(loginScreen.this, false, "Login Successful");
+                    //Toast.makeText(loginScreen.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
 
-                    Intent intent=new Intent(loginScreen.this, homeScreen.class);
+                    Intent intent = new Intent(loginScreen.this, homeScreen.class);
                     startActivity(intent);
-                }else
+                } else {
                     //incorrect
-                    Toast.makeText(loginScreen.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-            }   ;
-
+                    new extraFunctions.ShowToast(loginScreen.this, true, "ERROR: Login Failed!");;
+                }
+            }
         });
     }
 }
