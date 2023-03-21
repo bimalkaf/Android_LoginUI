18a54cb1fda6313a26f72a8a5844939c427096b7
kazol196295 <kazol196295@gmail.com>
2023-03-21 19:00:33 +0600

    Kuet getter and setter class added

---
+++ app/src/main/java/com/example/myloginapp/Kuet.java
@@ -0,0 +1,27 @@
+package com.example.myloginapp;
+
+public class Kuet {
+    String name;
+    int roll;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public Kuet(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+}
