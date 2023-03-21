4707e2c76b1abbbafdbb6c611e45bda4b0def71b
Md Kawsar Ahmed Kazol <kazol196295@gmail.com>
2023-03-21 23:05:52 +0600

    Update third.java

--- app/src/main/java/com/example/myloginapp/third.java
+++ app/src/main/java/com/example/myloginapp/third.java
@@ -1,1 +1,35 @@
+package com.example.myloginapp;
 
+public class Second {
+    int roll;
+    String name;
+
+
+    public Second(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+        int kazol;
+//        pera nai chiil
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+
+
+}
