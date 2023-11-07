c9651d6ef5a32646d177f80c5bbd90a914ec5c8a
fabio <fabiorigote913@gmail.com>
2023-11-07 09:56:46 -0300

    UPDATE 3

---
+++ app/src/main/java/com/example/myloginapp/SliderAdapter.java
@@ -0,0 +1,79 @@
+package com.example.myloginapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation. NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int imagesArray[] ={
+            R.drawable.onboardscreen1,
+            R.drawable.onboardscreen2,
+            R.drawable.onboardscreen3
+
+    };
+
+    int headingArray [] ={
+            R.string.first_slide,
+            R.string.second_slide,
+            R.string.third_slide
+    };
+
+    int descriptionArray [] ={
+            R.string.description,
+            R.string.description,
+            R.string.description
+    };
+
+
+    @Override
+    public int getCount() {
+        return headingArray.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.sliding_layout,container,false);
+
+        ImageView imageView = view.findViewById(R.id.slider_img);
+        TextView heading = view.findViewById(R.id.heading);
+        TextView description = view.findViewById(R.id.description);
+
+        imageView.setImageResource(imagesArray[position]);
+        heading.setText(headingArray[position]);
+        description.setText(descriptionArray[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+        container.removeView((ConstraintLayout)object);
+    }
+}
